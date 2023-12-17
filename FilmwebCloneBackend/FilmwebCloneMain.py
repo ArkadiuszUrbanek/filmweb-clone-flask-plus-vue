afb98562ef0b53099bda96d13423a057bd3fceb2
ArkadiuszUrbanek <56248627+ArkadiuszUrbanek@users.noreply.github.com>
2023-12-17 02:27:06 +0100

    ...

--- FilmwebCloneBackend/FilmwebCloneMain.py
+++ FilmwebCloneBackend/FilmwebCloneMain.py
@@ -1,17 +1,46 @@
-# Dołączanie modułu flask 
-from flask import Flask
-from flask import render_template, request, redirect, url_for, flash
+from flask import Flask, url_for, session
+from authlib.integrations.flask_client import OAuth
+from dotenv import dotenv_values
 
-# Tworzenie aplikacji
-app = Flask("FilmwebApp")
+app = Flask('FilmwebCloneBackend')
+app.secret_key = 'very hard to guess key'
 
+oauth = OAuth(app)
 
-@app.route('/', methods=['GET', 'POST'])
+config = dotenv_values('.env')
+google = oauth.register(name = 'google',
+                        client_id = config['GOOGLE_OAUTH2_CLIENT_ID'],
+                        client_secret = config['GOOGLE_OAUTH2_SECRET_KEY'],
+                        server_metadata_url = 'https://accounts.google.com/.well-known/openid-configuration',
+                        #access_token_url = 'https://accounts.google.com/o/oauth2/token',
+                        #access_token_url = 'https://oauth2.googleapis.com/token',
+                        #access_token_params = None,
+                        #authorize_url = 'https://accounts.google.com/o/oauth2/auth',
+                        #authorize_url = 'https://accounts.google.com/o/oauth2/v2/auth',
+                        #authorize_params = None,
+                        #api_base_url = 'https://www.googleapis.com/oauth2/v1/',
+                        #userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',
+                        #userinfo_endpoint = 'https://openidconnect.googleapis.com/v1/userinfo',
+                        client_kwargs = { 
+                            'scope': 'openid email profile', 
+                            #'redirect_url': 'http://localhost:5000/login/google/callback'                 
+                        })
+
+@app.route('/')
 def index():
-    # Przesłanie w odpowiedzi stworzonego widoku z pliku HTML (pliki muszą znajdować w folderze "templates")
-    return "Hello World!<br>"
+    return 'hello'
 
+@app.route('/login/google')
+def requestGoogleLogin():
+    google = oauth.create_client('google')
+    return google.authorize_redirect(redirect_uri = url_for('googleCallback', external = True))
 
-# Uruchomienie applikacji w trybie debug
-app.debug = True
-app.run()
+@app.route('/login/google/callback')
+def googleCallback():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    response = google.get('userinfo', token = token)
+    userInfo = response.json()
+    print(userInfo)
+
+app.run(host = '127.0.0.1', port = 5000, debug = True)
