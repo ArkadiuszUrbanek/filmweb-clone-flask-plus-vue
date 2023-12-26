91a07b9e87fff3c28552b9ce79e6e86dd1df77b9
ArkadiuszUrbanek <56248627+ArkadiuszUrbanek@users.noreply.github.com>
2023-12-26 21:06:07 +0100

    fixed code formating

--- FilmwebCloneBackend/app.py
+++ FilmwebCloneBackend/app.py
@@ -3,17 +3,17 @@ from models import db
 from blueprints import oauth, auth_blueprint
 from dotenv import dotenv_values
 
-config = dotenv_values('.env')
+if __name__ == '__main__':
+    config = dotenv_values('.env')
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = 'very hard to guess key'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{config["MYSQL_USERNAME"]}:{config["MYSQL_PASSWORD"]}@localhost/{config["MYSQL_DATABASE_NAME"]}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = config['APP_SECRET_KEY']
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{config["MYSQL_USERNAME"]}:{config["MYSQL_PASSWORD"]}@localhost/{config["MYSQL_DATABASE_NAME"]}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
-oauth.init_app(app)
+    db.init_app(app)
+    oauth.init_app(app)
 
-if __name__ == "__main__":
     with app.app_context(): 
         db.create_all()
     
--- FilmwebCloneBackend/models/user.py
+++ FilmwebCloneBackend/models/user.py
@@ -1,5 +1,4 @@
 from . import db
-from sqlalchemy import CheckConstraint
 from .utils.utcnow import utcnow
 
 class User(db.Model):
@@ -7,5 +6,5 @@ class User(db.Model):
     first_name = db.Column(db.String(20), nullable = False)
     last_name = db.Column(db.String(40), nullable = False)
     email = db.Column(db.String(40), nullable = False)
-    account_type = db.Column(db.String(8), CheckConstraint('account_type IN (\'app\', \'facebook\', \'google\')'), nullable = False)
+    account_type = db.Column(db.String(8), db.CheckConstraint('account_type IN (\'app\', \'facebook\', \'google\')'), nullable = False)
     creation_date = db.Column(db.DateTime, nullable = False, server_default = utcnow())
