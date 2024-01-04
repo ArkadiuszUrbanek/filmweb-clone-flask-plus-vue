0aefe1fce6732e72048ee60eda26abbdca3bdbbf
Tomasz Cichoń <tomasz.cichon@itsilesia.com>
2024-01-04 18:06:42 +0100

    fix: removed entity class

--- FilmwebCloneBackend/models/blueprints/entity.py
+++
@@ -1,7 +0,0 @@
-from .. import db
-from ..utils import utcnow
-
-#class Entity():
-#   id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-#   creation_date = db.Column(db.DateTime, nullable = False, server_default = utcnow())
-#   modification_date = db.Column(db.DateTime, nullable = False, server_default = utcnow())
